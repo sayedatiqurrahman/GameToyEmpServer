51bb7707542b0d4d6090c19febd69e331c70ddfe
Atiqur Rahman <s.atiqurrahman2003@gmail.com>
2023-05-18 14:09:50 +0600

    setup server

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express()
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json())
+app.get('/', (req, res, next) => {
+    res.send('you are successfully Connected')
+})
+
+app.listen(port, () => {
+    console.log('App listening on port', port);
+})
