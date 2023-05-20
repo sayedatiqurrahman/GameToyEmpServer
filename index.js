d87f76eedb7310d68b17bbd9570cec320f7b1c15
Atiqur Rahman <s.atiqurrahman2003@gmail.com>
2023-05-20 11:57:23 +0600

    create single toy details route

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const express = require('express');
 const app = express()
@@ -7,6 +7,7 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.json())
 // app.use(express.json());
 
 
@@ -51,6 +52,15 @@ async function run() {
                 ]
             }
             const result = await myToys.find(filter).toArray()
+            res.send(result)
+        })
+        app.get('/ToyDetails/:id', async (req, res) => {
+            const id = req.params
+            console.log(id);
+            const query = { _id: new ObjectId(id) }
+
+
+            const result = await myToys.findOne(query)
             res.send(result)
         })
         // Send a ping to confirm a successful connection
