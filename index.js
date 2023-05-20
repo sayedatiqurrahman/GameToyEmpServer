df02a9ae3998418332e2555d931113b0240176c1
Atiqur Rahman <s.atiqurrahman2003@gmail.com>
2023-05-20 21:03:18 +0600

    added search route to get data wehn anybody search in all toys page

--- index.js
+++ index.js
@@ -3,19 +3,13 @@ require('dotenv').config()
 const express = require('express');
 const app = express()
 const cors = require('cors');
-const corsOptions = {
-    origin: '*',
-    credentials: true,
-    optionSuccessStatus: 200,
-}
 const port = process.env.PORT || 5000;
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@atiqurrahman.ac8ixft.mongodb.net/?retryWrites=true&w=majority`;
 
-// console.log(uri);
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
@@ -31,7 +25,6 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        // client.connect();
 
         const myToys = client.db('gameToys').collection('myToys')
         //    Get all data || home route
@@ -69,16 +62,15 @@ async function run() {
             res.send(result)
         })
 
-        app.post('/search', async (req, res) => {
+        app.get('/search/:text', async (req, res) => {
+            const search = req.params.text;
+            const result = await myToys.find(
+                { name: { $regex: search, $options: "i" } }).toArray()
+            res.send(result)
 
         })
 
-        // Send a ping to confirm a successful connection
-        // await client.db("admin").command({ ping: 1 });
-        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
-        // Ensures that the client will close when you finish/error
-        // await client.close();
     }
 }
 run().catch(console.dir);
